5fd5952cfb47e36bb9c6ca78228b49eff8579718
Roderigoshq <rodrigoshiraquadros@gmail.com>
2024-04-22 09:19:40 -0300

    :feat: arrumando codigo simples

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:software/pages/login.dart';
+import 'package:flutter_application_1/pages/login.dart';
+
 
 void main() {
   runApp(const MyApp());
