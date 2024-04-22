10c82fa68d850ec85319702115ebc977bfc25894
Roderigoshq <rodrigoshiraquadros@gmail.com>
2024-04-22 19:55:38 -0300

    :feat: iniciando segunda pagina

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,9 +5,36 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Olá mundo!"),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 40, 0, 25),
+            color: Colors.white,
+            child: const Center(
+              child: Text(
+                "YE Gestão De Saúde",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 21,
+                  color: Color.fromRGBO(136, 149, 83, 1),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 0, 0, 45),
+            color: Colors.white,
+            child: Center(
+              child: Image.asset(
+                'lib/assets/Logo_gestao_de_saude.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
