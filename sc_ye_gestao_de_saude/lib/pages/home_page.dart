a467922153bd2c39e39588e714583f60a803f612
pauloVesperoo <paulovcvespero@gmail.com>
2024-04-30 11:37:21 -0300

    Comecei a fazer o home_page

--- sc_ye_gestao_de_saude/lib/pages/home_page.dart
+++ sc_ye_gestao_de_saude/lib/pages/home_page.dart
@@ -3,39 +3,132 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('YE Gestão de Saúde'),
+    return MaterialApp(
+      title: 'Index Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
+      home: IndexScreen(),
+    );
+  }
+}
+
+class IndexScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100), // Aumentando o tamanho da AppBar
+        child: AppBar(
+          backgroundColor: Color(0xFF889553), // Cor de fundo da AppBar
+          actions: [
+            Spacer(), // Adiciona um espaçamento flexível entre os ícones e o texto
+            IconButton(
+              icon: Icon(
+                Icons.info,
+                color: Color(0xFFC6D687), // Cor do ícone
+              ),
               onPressed: () {
-                // Ação para a seção de exames
-                Navigator.pushNamed(context, '/exames');
+                // Ação ao clicar no ícone de informação
               },
-              child: Text('Exames'),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Color(0xFFC6D687), // Cor do ícone
+              ),
               onPressed: () {
-                // Ação para a seção de consultas
-                Navigator.pushNamed(context, '/consultas');
+                // Ação ao clicar no ícone de configuração
               },
-              child: Text('Consultas'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Ação para a seção de medicação
-                Navigator.pushNamed(context, '/medicacao');
-              },
-              child: Text('Medicação'),
             ),
           ],
+          title: Text(
+            'YE Gestão de Saúde',
+            style: TextStyle(
+              color: Color(0xFFC6D687), // Cor do texto
+              fontSize: 24, // Tamanho do texto aumentado
+            ),
+          ),
         ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20), // Adicionando o padding
+        child: Container(
+          color: Colors.white, // Fundo em branco
+          child: Column(
+            children: [
+              // Adiciona a parte inferior da tela
+              BottomContent(
+                clienteNome: 'Usuário',
+                imagePath: 'assets/user_image.png',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomContent extends StatelessWidget {
+  final String clienteNome;
+  final String imagePath;
+
+  const BottomContent({
+    required this.clienteNome,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      color: const Color(0xFFFFFFFF), // Cor de fundo
+      child: Row(
+        children: [
+          // Texto à esquerda
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Olá, ',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0), // Cor do texto
+                        fontSize: 20, // Tamanho do texto aumentado
+                      ),
+                    ),
+                    Text(
+                      clienteNome,
+                      style: TextStyle(
+                        color: Color(0xFF889553), // Cor do texto
+                        fontSize: 20, // Tamanho do texto aumentado
+                        fontWeight: FontWeight.bold, // Deixando o texto em negrito
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5), // Espaçamento entre os textos
+                // Texto adicional
+                Text(
+                  'Veja seus dados abaixo',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0), // Cor do texto
+                    fontSize: 16, // Tamanho do texto aumentado
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 10), // Espaçamento entre o texto e a imagem
+          // Imagem à direita
+          CircleAvatar(
+            radius: 30, // Aumenta o tamanho da imagem
+            backgroundImage: AssetImage(imagePath),
+          ),
+        ],
       ),
     );
   }
