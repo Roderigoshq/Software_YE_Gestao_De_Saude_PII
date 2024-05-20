162269d85f5802eb2a72247f6d8fd38a812ff801
pauloVesperoo <paulovcvespero@gmail.com>
2024-05-20 11:35:29 -0300

    arrumando front-end da pagina de medicação

--- sc_ye_gestao_de_saude/lib/pages/medication_page.dart
+++ sc_ye_gestao_de_saude/lib/pages/medication_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:sc_ye_gestao_de_saude/widgets/medication_modal.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sc_ye_gestao_de_saude/models/medication_model.dart';
 import 'package:sc_ye_gestao_de_saude/services/medication_service.dart';
+import 'package:sc_ye_gestao_de_saude/widgets/medication_modal.dart';
 
 class MedicationPage extends StatefulWidget {
-  const MedicationPage({super.key});
+  const MedicationPage({Key? key}) : super(key: key);
 
   @override
   _MedicationPageState createState() => _MedicationPageState();
@@ -19,13 +19,14 @@ class _MedicationPageState extends State<MedicationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Container(
-            padding: const EdgeInsets.fromLTRB(40, 35, 0, 0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 37),
             child: Row(
               children: [
-                const Expanded(
+                Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -59,14 +60,14 @@ class _MedicationPageState extends State<MedicationPage> {
               ],
             ),
           ),
-          const SizedBox(height: 16),
+          SizedBox(height: 16),
           Expanded(
             child: ExtensionPanelMedication(),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
         onPressed: () {
           mostrarModelMedication(context);
         },
